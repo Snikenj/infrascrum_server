6fbb714100761d83414137c4ae384659be3816e6
LaynMDream <layn_mdream_@hotmail.com>
2022-07-25 16:19:53 +0200

    down

--- server/src/controllers/project.controller.ts
+++ server/src/controllers/project.controller.ts
@@ -2,24 +2,25 @@
 import type { Request, Response } from 'express';
 import { projectRepository } from '../application.database.js';
 
-const findAllProjects = async (req:Request, res:Response) => {
+const findAllProjects = async (req: Request, res: Response) => {
   return res.json({ projects: await projectRepository.find() });
 };
 
-const createProject = async (req:Request, res:Response) => {
+const createProject = async (req: Request, res: Response) => {
   const project = projectRepository.create(req.body);
   await projectRepository.save(project);
   res.status(201).json({ project });
 };
 
-const findProjectById = async (req:Request, res:Response) => {
-  const { id } = req.params;
-  return res.json({ project: await projectRepository.find(id) }); //! Trouver ID
-};
+// const findProjectById = async (req:Request, res:Response) => {
+//   const { id } = req.params;
+//   return res.json({ project: await projectRepository.find(id) }); //! Trouver ID
+// };
 
-const deleteProjectById = async (req:Request, res:Response) => {
+const deleteProjectById = async (req: Request, res: Response) => {
   const { id } = req.params;
   return res.json({ project: projectRepository.softDelete(id) });
 };
 
-export { createProject, findAllProjects, findProjectById, deleteProjectById };
+export { createProject, findAllProjects, deleteProjectById };
+// findProjectById,
