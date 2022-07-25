480df69d0a045b7132e3a82e3220613c397761f7
LaynMDream <layn_mdream_@hotmail.com>
2022-07-25 16:21:54 +0200

    ok

--- server/src/routes/project.router.ts
+++ server/src/routes/project.router.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { createProject, findAllProjects, deleteProjectById, findProjectById } from '../controllers/project.controller.js';
+import { createProject, findAllProjects, deleteProjectById } from '../controllers/project.controller.js';
 import { catchErrors } from '../middlewares/error.middleware.js';
 import { validate } from '../validators/base.validator.js';
 import { getProjectValidationRules } from '../validators/project.validator.js';
@@ -8,7 +8,7 @@ import { apiUserRouter } from './user.router.js';
 const apiProjectRouter:Router = Router();
 apiUserRouter.post('/projects/newproject', getProjectValidationRules(), validate, catchErrors(createProject)); //! A vérifier également
 apiUserRouter.get('/projects', catchErrors(findAllProjects));
-apiUserRouter.get('/projects/projects/:id', catchErrors(findProjectById));
+// apiUserRouter.get('/projects/projects/:id', catchErrors(findProjectById));
 apiUserRouter.delete('/projects/projects/:id', catchErrors(deleteProjectById)); //! Route à vérifier /pro/pro
 
 export { apiProjectRouter };
