b62a44e51b3af4bff04aff82e9721712130c22dc
Snikenj <soumsoum6991@gmail.com>
2022-07-29 12:36:03 +0200

    login update

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -2,7 +2,7 @@ import type { Request, Response } from 'express';
 import { userRepository } from '../application.database.js';
 
 const registerUser = async (req:Request, res:Response) => {
-  const user = userRepository.create(req.body);
+  const user = userRepository.create(req.body.user);
   await userRepository.save(user);
   res.json({ user });
 };
--- server/src/routes/user.router.ts
+++ server/src/routes/user.router.ts
@@ -2,9 +2,9 @@ import { Router } from 'express';
 import { registerUser } from '../controllers/user.controller.js';
 import { catchErrors } from '../middlewares/error.middleware.js';
 import { validate } from '../validators/base.validator.js';
-import { getUserValidationRules } from '../validators/register.validator.js';
+// import { getUserValidationRules } from '../validators/register.validator.js';
 
 const apiUserRouter:Router = Router();
-apiUserRouter.post('/users', getUserValidationRules(), validate, catchErrors(registerUser));
+apiUserRouter.post('/users', validate, catchErrors(registerUser));
 
 export { apiUserRouter };
