cc043af55b6616d46c0b1d79e01d46b9446528fc
Snikenj <soumsoum6991@gmail.com>
2022-08-04 10:50:31 +0200

    admin db relation to user

--- server/src/models/project.model.ts
+++ server/src/models/project.model.ts
@@ -5,17 +5,17 @@ import type { User } from './user.model.js';
 
 @Entity('projects')
 class Project extends BaseModel {
-    @Column({
-      nullable: false,
-    })
-  public admin!: number;
+  //   @Column({
+  //     nullable: false,
+  //   })
+  // public admin!: number;
 
     @Column({
       nullable: false,
       length: 50,
       unique: true,
     })
-    public projectName!: string;
+  public projectName!: string;
 
     @Column()
     public startDate!: Date;
@@ -31,6 +31,9 @@ class Project extends BaseModel {
 
     @ManyToOne('User', 'projects')
     public users?: User[];
+
+    @ManyToOne('User', 'projects_admins')
+    public admin?: User[];
 
   @OneToMany('Element', 'project')
     public elements?: Element[];
